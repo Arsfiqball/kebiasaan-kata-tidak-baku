a784458a6ed41dca5621cd25b81467e196cbcd81
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2016-10-25 12:35:44 +0700

    List of wrong words

---
+++ words.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    regex: /\banda\b/,
+    info: "Kata \"anda\" harus menggunakan kapital di awal menjadi \"Anda\"."
+  },
+  {
+    regex: /\bperubah\b/,
+    info: "Kata \"perubah\" tidak baku. Gunakan kata \"pengubah\"."
+  },
+  {
+    regex: /\bPerubah\b/,
+    info: "Kata \"Perubah\" tidak baku. Gunakan kata \"Pengubah\"."
+  },
+  {
+    regex: /\bmerubah\b/,
+    info: "Kata \"merubah\" tidak baku. Gunakan kata \"mengubah\"."
+  },
+  {
+    regex: /\bMerubah\b/,
+    info: "Kata \"Merubah\" tidak baku. Gunakan kata \"Mengubah\"."
+  },
+];
